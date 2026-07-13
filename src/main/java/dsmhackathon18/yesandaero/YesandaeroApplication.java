16fb00c6cf4c2a7154a148df24b2e0cca9b06b1f
tlgms <siheun9@gmail.com>
2026-07-13 13:55:32 +0900

    초기: Spring Boot 프로젝트 설정

---
+++ src/main/java/dsmhackathon18/yesandaero/YesandaeroApplication.java
@@ -0,0 +1,13 @@
+package dsmhackathon18.yesandaero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesandaeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesandaeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsmhackathon18/yesandaero/YesandaeroApplicationTests.java
@@ -0,0 +1,13 @@
+package dsmhackathon18.yesandaero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesandaeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
